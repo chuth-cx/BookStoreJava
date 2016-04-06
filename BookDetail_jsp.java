5af77ea722a18c470634545bf1adbba4e5598dd8
chuth-cx <Carsten.Huth@checkmarx.com>
2016-04-06 17:00:19 +0100

    Update BookDetail_jsp.java

--- BookDetail_jsp.java
+++ BookDetail_jsp.java
@@ -5,7 +5,7 @@ import org.apache.jasper.runtime.*;
 
 public class BookDetail_jsp extends HttpJspBase {
 
-
+// changed 06 Apr 2016
 //
 //   Filename: Common.jsp
 //   Generated with CodeCharge  v.1.2.0
