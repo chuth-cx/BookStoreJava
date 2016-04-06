cf0803cdc2dc6713152da6727e7d03477b3d7fa1
chuth-cx <Carsten.Huth@checkmarx.com>
2016-04-06 15:54:36 +0100

    Update AdminMenu_jsp.java

--- AdminMenu_jsp.java
+++ AdminMenu_jsp.java
@@ -6,6 +6,7 @@ import org.apache.jasper.runtime.*;
 public class AdminMenu_jsp extends HttpJspBase {
 
 // modified 06 Aug 16
+// modified 06 Aug 16 (2)
 //
 //   Filename: Common.jsp
 //   Generated with CodeCharge  v.1.2.0
